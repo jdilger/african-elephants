8b15cbe4244d0d7f3ea77867736b58bd9881b765
John Dilger <johnjdilger@gmail.com>
2020-04-13 15:08:04 -0700

    added veg monitoring function

--- main.py
+++ main.py
@@ -59,4 +59,44 @@ class Vegetation(base):
 	def __inti__(self):
 		super(Vegetation, self).__init__()
 
-	
+	def monthlyNDVI(self, m, y, ic):
+		"""
+		Monthly NDVI(NDVIi, m, y) for each Monitoring Unit (MU) i
+		in month m and year y is obtained by averaging the 3 dekadal values in each month
+		full text: https://www.frontiersin.org/articles/10.3389/fenvs.2019.00187/full
+		@param self:
+		@param m: month as integer
+		@param y: year as integer
+		@param ic: NDVI image collection
+		@return: Four band image with 2 month NDVI mean, anomaly, standard anomaly, and Vegetative Control Index
+		"""
+
+
+		ic = ic.filter(ee.Filter.calendarRange(m, m, 'month'))
+
+		month_i_ic = ic.filter(ee.Filter.calendarRange(y, y, 'year'))
+
+		month_i_mean = month_i_ic.mean().rename('NDVI_mean')
+
+		month_i_mean_std = ee.Image.constant(month_i_mean_std.get('NDVI_mean'))
+
+
+		baseline_ic = ic.filter(ee.Filter.calendarRange(y, y - 10, 'year'))
+
+		baseline_mean = baseline_ic.mean()
+
+
+		aandvi = month_i_mean.subtract(baseline_mean).rename('AANDVI')
+
+		sandvi = aandvi.divide(ee.Image(month_i_mean_std)).rename('SANDVI')
+
+		vci_min = baseline_mean.reduceRegion(
+			{reducer: ee.Reducer.min(), geometry: geometry, scale: 30, bestEffort: true, maxPixels: 1e13})
+		vci_max = baseline_mean.reduceRegion(
+			{reducer: ee.Reducer.max(), geometry: geometry, scale: 30, bestEffort: true, maxPixels: 1e13})
+		vci_min = ee.Image.constant(vci_min.get('nd'))
+		vci_max = ee.Image.constant(vci_max.get('nd'))
+		vci = month_i_mean.subtract(vci_min).divide(vci_max.subtract(vci_min)).rename('VCI')
+
+		return ee.Image.cat([month_i_mean, aandvi, sandvi, vci])
+
