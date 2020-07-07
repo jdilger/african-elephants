7d0dfd9f14940a87acbec4e899c3cd7f59433b19
John Dilger <johnjdilger@gmail.com>
2020-07-06 20:24:23 -0700

    reformated parameters, adjusted date vars

--- run.py
+++ run.py
@@ -4,13 +4,6 @@ import main
 import datetime
 ee.Initialize()
 
-# Date time parameters
-# parameters for ndvi anomallies
-py_date = datetime.datetime.utcnow()
-
-sd = ee.Date(py_date).advance(-2,'month')
-ed = ee.Date(py_date)
-
 # region of interest
 region = ee.Geometry.Polygon([[[22.37055462536107, -19.69234130304949],
                                    [23.161822166438526, -19.675148989974225],
@@ -22,7 +15,7 @@ region = ee.Geometry.Polygon([[[22.37055462536107, -19.69234130304949],
 # fire history length, list [integer, string],
 # integer = unit of time to to collected fire data
 # string = time unit of 'months','days','years'
-fireHistoryLength = [-6, 'months']
+fireHistoryLength = [-6, 'month']
 
 # parameters for vegetation products
 # list [integer, string],
@@ -31,14 +24,30 @@ fireHistoryLength = [-6, 'months']
 # string = time unit of 'years'
 vegetationHistoryLength = [5,'year']
 
+# parameters for water products
+# list [integer, string],
+# duration to include in water availability mapping
+# integer = unit of time to include
+# string = time unit of 'months','days','years'
+waterHistoryLength = [-2,'month']
+
 # Export information
 bucket = 'wwf-gee-bucket'
-fireAsset = ''
+# export to asset base path
+assetbase = "users/TEST"
+
 # # export to cloud
 fireFolder = 'AfricanElephants/fireAssets'
 waterFolder = 'AfricanElephants/waterAssets'
 vegetationFolder = 'AfricanElephants/vegetationAssets'
 
+
+# Date time parameters
+py_date = datetime.datetime.utcnow()
+
+# move back one day to the last date of previous month
+# ed = ee.Date(py_date).advance(-1,'day')
+ed = ee.Date('2019-01-01')
 
 # Fire products
 fire = main.Fire().burnOut(ed.advance(-1,'month'), -2, 'month').toInt16()
@@ -47,8 +56,6 @@ historyFire = main.Fire().historyFire(historyFireStartDate,ed)
 
 fire = fire.addBands(historyFire).select(['confidence', 'allFires', 'past', 'distance10k', 'historyFire'])
 
-print(fire.bandNames().getInfo(),historyFire.bandNames().getInfo())
-
 # Vegetation products
 def nd(image):
     out = image.normalizedDifference(['nir', 'red']).copyProperties(image, ["system:time_start"])
@@ -56,22 +63,26 @@ def nd(image):
 
 ic = main.landsat().preprocess(startDate=ee.Date('2000-01-01'),endDate=ed,studyArea=region).select(['blue', 'green', 'red', 'nir', 'swir1', 'swir2']).map(nd)
 
-m = sd.get('month').getInfo()
-y = sd.get('year').getInfo()
+m = ed.get('month').getInfo()
+y = ed.get('year').getInfo()
 vegetation = main.Vegetation().byRegion(m, y, ic, region, vegetationHistoryLength)
 
 # Water products
+sd = ee.Date(ed).advance(waterHistoryLength[0],waterHistoryLength[1])
 sen2 = main.sentinel2().preprocess(startDate=sd,endDate=ed,studyArea=region).select(['blue', 'green', 'red', 'nir', 'swir1', 'swir2'])
-img = main.Water().wlc(sen2)
+img = main.Water().wlc(sen2, startDate=sd,endDate=ed)
 water = main.Water().wlcexpression(img, region)
 
+# Exporting
+datestr = py_date.strftime("%Y%m%d")
+waterDesc = '%s%s' % ('water_',datestr)
+vegDesc = '%s%s' % ('vegetation_',datestr)
+fireDesc = '%s%s' % ('fire_',datestr)
 
 # main.base().exportMapToCloud(water,'watertest',region,bucket,prefix=waterFolder)
 # main.base().exportMapToCloud(vegetation,'vegetationtest',region,bucket,prefix=vegetationFolder,scale=30)
 # main.base().exportMapToCloud(fire,'firetest',region,bucket,prefix=fireFolder,scale=500)
 
-# export to asset
-# assetbase = "users/TEST"
-# main.base().exportMapToAsset(water,'watertestass',region,assetbase)
-# main.base().exportMapToAsset(vegetation,'vegetationtest2',region,assetbase,scale=30)
-# main.base().exportMapToAsset(fire,'firetestass',region,assetbase,scale=500)
+main.base().exportMapToAsset(water,waterDesc,region,assetbase,scale=10)
+main.base().exportMapToAsset(vegetation,vegDesc,region,assetbase,scale=30)
+main.base().exportMapToAsset(fire,fireDesc,region,assetbase,scale=500)
